42231c27756c880459ffd8ef3e59a156b4bd14b5
Uday <udayakumaryu@gmail.com>
2021-04-19 09:08:40 +0530

    final commit by EST Guy

---
+++ src/test/java/qaclickacademy/Demo.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void Appiumone() 
+	{
+		System.out.println("Appiumone");
+	}
+	@Test
+	public void Appiumtwo()
+	{
+		System.out.println("Appiumtwo");
+	}
+	@Test
+	public void Appiumthree()
+	{
+		System.out.println("Appiumtwo");
+	}
+
+}
